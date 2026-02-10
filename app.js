54c792db08a7b1a2ba11d16d5eeee6b442a94f91
Emanuel Villegas <fevillegas.dev@gmail.com>
2026-02-10 15:45:47 -0300

    ...

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const arButton = document.getElementById('arButton');
+const modelViewer = document.getElementById('viewer');
 
 arButton.addEventListener('click', () => {
-  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
   if (isIOS) {
-    openQuickLook();
+    const usdzSrc = modelViewer.getAttribute('ios-src');
+    if (usdzSrc) {
+      openQuickLook(usdzSrc);
+    } else {
+      alert('No se encontró un modelo AR para iOS.');
+    }
   } else {
     // Acá luego enchufamos WebXR para Android
     alert('AR en Android lo activamos en el próximo paso');
   }
 });
 
-function openQuickLook() {
+function openQuickLook(usdzSrc) {
   const link = document.createElement('a');
-  //link.rel = 'ar';
-  link.href = './model.usdz'; // IMPORTANTE: USDZ para Safari
+  link.rel = 'ar';
+  link.href = usdzSrc;
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
 }
-
-
